786cc01aa1f9dcff5d055fcc067248c2f602163d
amang06 <aman@skynetiks.com>
2024-11-10 23:24:05 +0530

    update regex

--- lib/replaceUrlInHtml.ts
+++ lib/replaceUrlInHtml.ts
@@ -1,5 +1,6 @@
 export function replaceUrlsInEmailHtml(campaign: { id: string; bodyHTML: string }, emailId: string) {
-  const linkPattern = /<a\s+[^>]*href="(https?:\/\/[^\s"'<>]+)"/gi;
+  // Matches URLs in href attributes, with or without "http" or "https" (e.g., "https://example.com" or "app.skyfunnel.ai")
+  const linkPattern = /<a\s+[^>]*href="((https?:\/\/|www\.|[a-zA-Z0-9\-]+\.[a-zA-Z]{2,})([^\s"'<>]*))"/gi;
 
   campaign.bodyHTML = campaign.bodyHTML.replace(linkPattern, (match, url) => {
     const encodedUrl = encodeURIComponent(url);
