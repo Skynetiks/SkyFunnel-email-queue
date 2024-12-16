4f5b7faba1c6024ca7266421da18cee240c07696
Diwanshu <saidiwanshu1880@gmail.com>
2024-12-16 18:53:41 +0530

    chore: improve singleton in pool

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,28 +8,65 @@ import { isDevelopment } from "./utils";
 const { Pool } = pg;
 dotenv.config();
 
+export let pool: pg.Pool | undefined;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const sslConfig = !isDevelopment
+const sslOptions = !isDevelopment
   ? {
       ca: fs.readFileSync(path.resolve(__dirname, "./certs/us-east-1-bundle.pem")),
     }
   : undefined;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslConfig,
-});
+function getPool() {
+  try {
+    if (pool) {
+      console.log("Using existing pool");
+      return pool;
+    }
 
+    console.log("Creating new pool");
+
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: sslOptions,
+      idleTimeoutMillis: 30000, // 30 seconds
+      max: 100, // Limit the number of concurrent connections
+    });
+
+    return pool;
+  } catch (error) {
+    console.error("Failed to create a connection pool:", error);
+    throw error; // Ensure the application knows about the critical failure
+  }
+}
+
+
+
+getPool()
 export const query = async (text: string, params: (string | number)[]) => {
   try {
-    const result = await pool.query(text, params);
+    const activePool = await getPool().connect();
+    if (!activePool) {
+      throw new Error("Database pool is not initialized");
+    }
+    const result = await activePool.query(text, params);
     return result;
   } catch (error) {
-    console.error(error);
+    console.error("Query execution failed:", error);
     throw error;
   }
 };
 
+
 export default pool;
+
+
+process.on("SIGINT", async () => {
+  if (pool) {
+    console.log("Closing database pool...");
+    await pool.end();
+    console.log("Database pool closed");
+  }
+  process.exit(0);
+});
