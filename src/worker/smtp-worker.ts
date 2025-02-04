b41b4f76f23f64e88175b6c534f8fb5c227264de
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2025-02-04 11:47:42 +0530

    fix: update email count

--- src/worker/smtp-worker.ts
+++ src/worker/smtp-worker.ts
@@ -116,12 +116,17 @@ async function sendEmailAndUpdateStatus(
         [email.emailCampaignId],
       );
 
+      const updateOrganizationResult = query(
+        'UPDATE "Organization" SET "sentEmailCount" = "sentEmailCount" + 1 WHERE id = $1',
+        [campaignOrg.id],
+      );
+
       const addDeliveryEventResult = query(
         'INSERT INTO "EmailEvent" ("id", "emailId", "eventType", "timestamp", "campaignId") VALUES (uuid_generate_v4(), $1, $2, $3, $4)',
         [email.id, "DELIVERY", new Date().toISOString(), email.emailCampaignId],
       );
 
-      await Promise.all([updateEmailResult, updateCampaignResult, addDeliveryEventResult]);
+      await Promise.all([updateEmailResult, updateCampaignResult, addDeliveryEventResult, updateOrganizationResult]);
     } else {
       console.error("[SMTP_WORKER] Error While Sending Emails via Smtp for", email.leadEmail, emailSent ? emailSent.response : "");
       throw new AppError("INTERNAL_SERVER_ERROR", "Email not sent by SMTP");
